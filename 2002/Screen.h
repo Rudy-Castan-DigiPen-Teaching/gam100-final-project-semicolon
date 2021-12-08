8e1c8898189748da720d9f90f016f8b6aa5d86a8
Hyunjun Lee <80097977+Hyun-jun-Lee0811@users.noreply.github.com>
2021-12-08 21:23:28 +0900

    Add files via upload

---
+++ 2002/Screen.h
@@ -0,0 +1,14 @@
+﻿// Filename : <Screen.h>
+// Authors : <Dawoon Jung, Hyunjun Lee>
+// Course : GAM100F21
+// All content © 2021 DigiPen(USA) Corporation, all rights reserved.
+
+#pragma once
+
+int current_scene = 4;
+int game_menu     = 0;
+int stage1 = 1;
+int stage2 = 2;
+int gameover = 3;
+int gamewin = 4;
+int howtoplay = 5;
